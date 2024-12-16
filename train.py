e235f53cc355a3a48be8c4ce124b44e62d3762b7
Amha Kindu <amhaznif@gmail.com>
2024-12-16 15:30:15 +0300

    early stopping bug fix

--- train.py
+++ train.py
@@ -17,15 +17,13 @@ from torch.utils.data.distributed import DistributedSampler
 
 class EarlyStopping:
     def __init__(self, patience=5, min_delta=0):
+        self.counter = 0
         self.patience = patience
         self.min_delta = min_delta
-        self.counter = 0
-        self.best_loss = None
+        self.best_loss = float('inf')
 
     def __call__(self, val_loss):
-        if self.best_loss is None:
-            self.best_loss = val_loss
-        elif val_loss < self.best_loss - self.min_delta:
+        if val_loss < self.best_loss - self.min_delta:
             self.best_loss = val_loss
             self.counter = 0
         else:
