added prefix masking & attention masking bug fix

dataset.py
@@ -1,5 +1,6 @@
 import os
 import json
+import ijson
 import torch
 import struct
 import random
@@ -10,8 +11,8 @@ from abc import ABC, abstractmethod
 from torch.utils.data import Dataset, Subset, DataLoader, Sampler, ConcatDataset, SequentialSampler
 
 from config import *
-from utils import Conversation
 from preprocessor import AmharicPreprocessor
+from utils import Conversation, get_casual_and_prefix_mask, get_casual_mask
 
 
 class NLPDataset(Dataset):
@@ -83,14 +84,20 @@ class TextDataset(NLPDataset):
                 if preprocessed_text:
                     self.samples.append(self.get_io_tensors(preprocessed_text))
                     if self.samples and len(self.samples) % 100000 == 0:
-                        LOGGER.info(f"\033[93mLoaded {len(self.texts)} samples from {file_name}\033[0m") if GLOBAL_RANK == COORDINATOR_RANK else None
-        LOGGER.info(f"\033[92mDone! Loaded {len(self.texts)} samples from {file_name}\033[0m") if GLOBAL_RANK == COORDINATOR_RANK else None
+                        LOGGER.info(f"\033[93mLoaded {len(self.samples)} samples from {file_name}\033[0m") if GLOBAL_RANK == COORDINATOR_RANK else None
+        LOGGER.info(f"\033[92mDone! Loaded {len(self.samples)} samples from {file_name}\033[0m") if GLOBAL_RANK == COORDINATOR_RANK else None
 
     def __len__(self) -> int:
         return len(self.samples)
 
     def __getitem__(self, index) -> dict:
-        return self.samples[index]
+        input, output = self.samples[index]
+        
+        # (SEQ_LEN,) != (1,) --> (SEQ_LEN,) --> (1, SEQ_LEN) --> (1, SEQ_LEN) & (1, SEQ_LEN, SEQ_LEN) --> (1, SEQ_LEN, SEQ_LEN)
+        mask = (input != self.pad_token).unsqueeze(0) & \
+            get_casual_mask(self.max_len)
+        
+        return input, output, mask
     
 
 class TextStreamDataset(NLPDataset):
@@ -125,12 +132,19 @@ class TextStreamDataset(NLPDataset):
                         LOGGER.info(f"\033[93mRegistered {count} offsets from {file_name}\033[0m") if GLOBAL_RANK == COORDINATOR_RANK else None
                     
                 with open(meta_path, 'w') as f:
-                    json.dump({ "mtime": os.path.getmtime(file_path), "tokens": self.tokens }, f, indent=2)
+                    json.dump({ "mtime": os.path.getmtime(file_path), "tokens": self.tokens, "samples": count }, f, indent=2)
             
-        # Read-only memmap of uint64 offsets of each sample
-        self.offsets = np.memmap(index_path, dtype=np.uint64, mode='r')
-        LOGGER.info(f"\033[92mDone! Registered {len(self.offsets)} offsets from {file_name}\033[0m") if GLOBAL_RANK == COORDINATOR_RANK else None
+            LOGGER.info(f"\033[92mDone! Registered {count} offsets from {file_name}\033[0m") if GLOBAL_RANK == COORDINATOR_RANK else None
+        else:
+            LOGGER.info(f"\033[93mUsing existing index for {file_name}\033[0m") if GLOBAL_RANK == COORDINATOR_RANK else None
         
+        # Read-only memmap of uint64 offsets of each sample        
+        self.offsets = np.memmap(index_path, dtype=np.uint64, mode='r')
+        if GLOBAL_RANK == COORDINATOR_RANK:
+            index_file = os.path.basename(index_path)
+            mib = self.offsets.nbytes / (1024 ** 2)
+            LOGGER.info(f"\033[92mEstablished read-only memory mapping with {index_file} ({mib:.2f} MiB) for {len(self.offsets)} offsets\033[0m")
+    
     def __len__(self) -> int:
         return len(self.offsets)
         
@@ -140,9 +154,15 @@ class TextStreamDataset(NLPDataset):
             f.seek(offset)
             line = f.readline()       
         
-        return self.get_io_tensors(
+        input, output = self.get_io_tensors(
             text=json.loads(line.strip())
         )
+        
+        # (SEQ_LEN,) != (1,) --> (SEQ_LEN,) --> (1, SEQ_LEN) --> (1, SEQ_LEN) & (1, SEQ_LEN, SEQ_LEN) --> (1, SEQ_LEN, SEQ_LEN)
+        mask = (input != self.pad_token).unsqueeze(0) & \
+            get_casual_mask(self.max_len)
+        
+        return input, output, mask
 
 
 class FineTuningDataset(NLPDataset):
@@ -159,8 +179,7 @@ class FineTuningDataset(NLPDataset):
         file_name = os.path.basename(file_path)
         with open(file_path, 'r', encoding='utf-8') as f:
             LOGGER.info(f"\033[93mLoading data from {file_name}{'/' + intent if intent else ''}...\033[0m") if GLOBAL_RANK == COORDINATOR_RANK else None
-            data = json.load(f)
-            for sample in data:
+            for sample in ijson.items(f, 'item'):
                 if intent and sample['type'] != intent:
                     continue
                 system_prompt = sample.get("system", "")
@@ -177,8 +196,7 @@ class FineTuningDataset(NLPDataset):
                         LOGGER.error('File must be in JSON format [{"system": ..., "exchanges": [{"input": ..., "output": ...}, ...}]]')
                         exit(1)
                 try:
-                    input, output = self.get_io_tensors(conv)
-                    self.samples.append((input, output))
+                    self.samples.append(self.get_io_tensors(conv))
                 except ValueError:
                     skips += 1
                     continue
@@ -193,7 +211,24 @@ class FineTuningDataset(NLPDataset):
         return len(self.samples)
 
     def __getitem__(self, index) -> dict:
-        return self.samples[index]
+        input, output = self.samples[index]
+        
+        # (SEQ_LEN,) != (1,) --> (SEQ_LEN,) --> (1, SEQ_LEN) --> (1, SEQ_LEN) & (1, SEQ_LEN, SEQ_LEN) --> (1, SEQ_LEN, SEQ_LEN)
+        mask = (input != self.pad_token).unsqueeze(0) & \
+            get_casual_and_prefix_mask(self.max_len, self.get_prefixes(input))
+            
+        return input, output, mask
+    
+    def get_prefixes(self, input: torch.Tensor) -> list[tuple[int, int]]:
+        start = None
+        prefix_boundaries = []
+        for i in range(len(input)):
+            if start is None and input[i] in [self.user_token, self.system_token]:
+                start = i
+            elif input[i] == self.bot_token and start is not None:
+                prefix_boundaries.append((start, i - 1))
+                start = None
+        return prefix_boundaries
     
     def get_io_tensors(self, conv: Conversation) -> tuple[torch.Tensor, torch.Tensor]:
         # System:              K L M N O
@@ -242,7 +277,7 @@ class FineTuningDataset(NLPDataset):
         
         input_ids.extend(exchanges_ipt)
         output_ids.extend(exchanges_opt)
-
+        
         padding = self.max_len - len(input_ids)
         
         # (SEQ_LEN,)
@@ -282,7 +317,18 @@ class MultiTaskDataset(Dataset):
         self.total_len = total
 
     def __len__(self):
-        return self.total_len
+        return self.total_len    
+    
+    def get_loader(self, batch_size: int, sampler: Sampler=None) -> DataLoader:
+        return DataLoader(
+            dataset=self,
+            batch_size=batch_size,
+            shuffle=(sampler is None),      # Sampler itself will handle shuffling if provided
+            sampler=sampler,
+            num_workers=self.workers,       # Number of subprocesses to use for data loading
+            pin_memory=True,                # pre-allocate batches in page-locked memory so that GPU transfers are faster and can be asynchronous
+            drop_last=True,                 # drop the last incomplete(does not have the size 'batch_size') batch
+        )
     
     def __getitem__(self, global_index: int):
         task_id = bisect_left(self.offsets, global_index)

finetune.py
@@ -13,7 +13,7 @@ from model import GPTmodel
 from tensorboard_logger import TensorboardLogger
 from lr_schedulers import LRScheduler, get_lr_scheduler
 from dataset import MultiTaskDataset, ShardedMultiTaskDataset,TemperatureSampler, FineTuningDataset, NLPDataset
-from utils import EarlyStopping, get_lookback_mask, log_confidence_metrics, save_checkpoint, set_trainable_params, validate
+from utils import EarlyStopping, log_confidence_metrics, save_checkpoint, set_trainable_params, validate
 
 
 def finetune(config: TrainingConfig, model: GPTmodel, finetune_dataset: NLPDataset, val_dataset: ShardedMultiTaskDataset, training_state: TrainingState | None = None) -> None:
@@ -23,7 +23,7 @@ def finetune(config: TrainingConfig, model: GPTmodel, finetune_dataset: NLPDatas
     tb_logger.log_text("ModelConfig", f"```json\n{json.dumps(model.config.__dict__, indent=2)}\n```", step=0)
     tb_logger.log_text("Environment", f"```json\n{json.dumps(ENV, indent=2)}\n```", step=0)
     
-    scaler = torch.GradScaler(device=DEVICE.type) if MIXED_PRECISION_ENABLED else None
+    scaler = torch.GradScaler(init_scale=config.grad_scaler_init, device=DEVICE.type) if MIXED_PRECISION_ENABLED else None
 
     early_stopping = EarlyStopping(patience=config.es_patience, min_delta=config.es_min_delta)
 
@@ -67,10 +67,10 @@ def finetune(config: TrainingConfig, model: GPTmodel, finetune_dataset: NLPDatas
             # (N_BATCHES, SEQ_LEN)
             decoder_input: torch.Tensor = batch[0].to(DEVICE)
             label: torch.Tensor         = batch[1].to(DEVICE)
-
-            # (SEQ_LEN, SEQ_LEN)
-            decoder_mask: torch.Tensor = get_lookback_mask(decoder_input.shape[1]).to(DEVICE)
-
+            
+            # (N_BATCHES, SEQ_LEN, SEQ_LEN)
+            decoder_mask: torch.Tensor  = batch[2].to(DEVICE)
+            
             with torch.autocast(device_type=DEVICE.type, enabled=MIXED_PRECISION_ENABLED):
                 # (N_BATCHES, SEQ_LEN, VOCAB_SIZE)
                 logits: torch.Tensor = model(decoder_input, decoder_mask)

inference.py
@@ -10,7 +10,7 @@ import torch.nn.functional as F
 from config import *
 from model import GPTmodel
 from cache import SlidingKVCache
-from utils import get_lookback_mask
+from utils import get_casual_and_prefix_mask, get_casual_mask
 from preprocessor import AmharicPreprocessor
 
 
@@ -93,8 +93,8 @@ class GptInferenceEngine:
                 token_ids,
                 dtype=torch.int64
             ).to(DEVICE).unsqueeze(0)
-                        
-            decoder_mask = get_lookback_mask(len(token_ids)).to(DEVICE)
+            
+            decoder_mask = get_casual_mask(len(token_ids)).to(DEVICE)
                         
             with torch.autocast(device_type=DEVICE.type, enabled=MIXED_PRECISION_ENABLED):
                 # (1, SEQ_LEN, VOCAB_SIZE)

model.py
@@ -1,6 +1,8 @@
 import math
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.attention import SDPBackend
 
 from config import *
 from lora import LoRAdapter
@@ -89,39 +91,26 @@ class MultiHeadAttentionBlock(nn.Module):
                 key_past, value_past = kv_cache
                 
                 # (N_BATCHES, CACHE_SIZE + SEQ_LEN, EMBED_DIM)
-                key = torch.cat([key_past, key], dim=2)
-                value = torch.cat([value_past, value], dim=2)
+                key = torch.cat([key_past, key], dim=1)
+                value = torch.cat([value_past, value], dim=1)
             kv_cache = key, value
 
         # (N_BATCHES, SEQ_LEN, EMBED_DIM) --> (N_BATCHES, SEQ_LEN, HEADS, d_head) --> (N_BATCHES, HEADS, SEQ_LEN, d_head)
         query = query.view(query.shape[0], query.shape[1], self.heads, -1).transpose(1, 2)
         key = key.view(key.shape[0], key.shape[1], self.heads, -1).transpose(1, 2)
         value = value.view(value.shape[0], value.shape[1], self.heads, -1).transpose(1, 2)
-
-        # (N_BATCHES, HEADS, SEQ_LEN, d_head) @ (N_BATCHES, HEADS, d_head, SEQ_LEN)
-        # (N_BATCHES, HEADS, SEQ_LEN, SEQ_LEN)
-        attention_scores = (query @ key.transpose(2, 3)) / math.sqrt(self.d_head)
         
-        assert mask.dtype == torch.bool, f"Mask must be boolean, got {mask.dtype}"
-        
-        if MIXED_PRECISION_ENABLED:
-            attention_scores = attention_scores.to(torch.float32)
-            
-        attention_scores.masked_fill_(mask == False, -1e09)
-
-        # (N_BATCHES, HEADS, SEQ_LEN, SEQ_LEN)
-        attention_scores = torch.softmax(
-            attention_scores, dim=-1
-        )
-
-        if MIXED_PRECISION_ENABLED:
-            attention_scores = attention_scores.to(torch.float16)
-
-        attention_scores = self.dropout(attention_scores)
-
-        # (N_BATCHES, head, SEQ_LEN, SEQ_LEN) @ (N_BATCHES, head, SEQ_LEN, d_head)
-        # (N_BATCHES, head, SEQ_LEN, d_head)
-        output: torch.Tensor = attention_scores @ value
+        with torch.nn.attention.sdpa_kernel([SDPBackend.EFFICIENT_ATTENTION, SDPBackend.MATH]):
+            # Step 1: attn_scores = (query @ key.transpose(2, 3)) / math.sqrt(self.d_head)
+            # Step 2: attn_scores.masked_fill_(mask == False, -1e09)
+            # Step 3: attn_probs = F.softmax(attn_scores, dim=-1)
+            # Step 4: output = (attn_probs @ value)
+            output = F.scaled_dot_product_attention(
+                query, key, value,
+                attn_mask=mask,
+                dropout_p=self.dropout.p if self.training else 0.0,
+                is_causal=(mask is None)
+            )
 
         # (N_BATCHES, head, SEQ_LEN, d_head) -> (N_BATCHES, SEQ_LEN, head, d_head)
         output = output.transpose(1, 2)

train.py
@@ -16,7 +16,7 @@ from model import GPTmodel
 from tensorboard_logger import TensorboardLogger
 from lr_schedulers import LRScheduler, get_lr_scheduler
 from dataset import ShardedDataset, TextStreamDataset, TextDataset, NLPDataset
-from utils import EarlyStopping, get_lookback_mask, log_confidence_metrics, save_checkpoint, validate
+from utils import EarlyStopping, get_casual_mask, log_confidence_metrics, save_checkpoint, validate
 
 
 def train(config: TrainingConfig, model: GPTmodel, train_dataset: NLPDataset, val_dataset: ShardedDataset, is_distributed: bool = False, training_state: TrainingState | None = None) -> None:
@@ -73,9 +73,9 @@ def train(config: TrainingConfig, model: GPTmodel, train_dataset: NLPDataset, va
             # (N_BATCHES, SEQ_LEN)
             decoder_input: torch.Tensor = batch[0].to(DEVICE)
             label: torch.Tensor         = batch[1].to(DEVICE)
-
-            # (SEQ_LEN, SEQ_LEN)
-            decoder_mask: torch.Tensor = get_lookback_mask(decoder_input.shape[1]).to(DEVICE)
+            
+            # (N_BATCHES, SEQ_LEN, SEQ_LEN)
+            decoder_mask: torch.Tensor  = batch[2].to(DEVICE)
 
             with torch.autocast(device_type=DEVICE.type, enabled=MIXED_PRECISION_ENABLED):
                 # (N_BATCHES, SEQ_LEN, VOCAB_SIZE)
@@ -226,6 +226,7 @@ if __name__ == "__main__":
     parser.add_argument("--resume", default=False, action="store_true", help="Resume training from checkpoint")
     parser.add_argument("--max-checkpoints-to-keep", type=int, help="Maximum number of checkpoints to keep")
     parser.add_argument("--dl-workers", type=int, help="Number of subprocesses to use for data loading")
+    parser.add_argument("--stream", default=False, action="store_true", help="Stream data from disk")
 
     args = parser.parse_args()
     
@@ -270,12 +271,19 @@ if __name__ == "__main__":
     tokenizer = spm.SentencePieceProcessor()
     tokenizer.LoadFromFile(args.tokenizer)
     
-    if os.path.getsize(training_config.training_data) > 200 * 1024 * 1024:
+    if args.stream or os.path.getsize(training_config.training_data) > 200 * 1024 * 1024:
         if GLOBAL_RANK == COORDINATOR_RANK:
             LOGGER.info(f"File '{os.path.basename(training_config.training_data)}' too large! streaming file...")
         train_dataset = TextStreamDataset(training_config.training_data, tokenizer, model_config.seq_len, training_config.workers)
     else:
         train_dataset = TextDataset(training_config.training_data, tokenizer, model_config.seq_len, training_config.workers)
+            
+    if args.stream or os.path.getsize(training_config.validation_data) > 200 * 1024 * 1024:
+        if GLOBAL_RANK == COORDINATOR_RANK:
+            LOGGER.info(f"File '{os.path.basename(training_config.validation_data)}' too large! streaming file...")
+        val_dataset = TextStreamDataset(training_config.validation_data, tokenizer, model_config.seq_len)
+    else:
+        val_dataset = TextDataset(training_config.validation_data, tokenizer, model_config.seq_len)
     
     training_config.samples_per_epoch = math.floor(len(train_dataset) / (training_config.batch_size * WORLD_SIZE))
     training_config.updates_per_epoch = math.floor(training_config.samples_per_epoch / training_config.grad_accum_steps)
@@ -283,7 +291,7 @@ if __name__ == "__main__":
     shards = training_config.updates_per_epoch // training_config.validate_every
     val_dataset = ShardedDataset(
         shards=shards,
-        dataset=TextDataset(training_config.validation_data, tokenizer, model_config.seq_len),
+        dataset=val_dataset,
         workers=training_config.workers,
     )
     
@@ -294,7 +302,7 @@ if __name__ == "__main__":
         LOGGER.info(f"Total training samples: {len(train_dataset)}")
         if train_dataset.tokens > 0:
             LOGGER.info(f"Total training tokens: {train_dataset.tokens}")
-            LOGGER.info(f"Average tokens per sample: {train_dataset.tokens / len(train_dataset)}")
+            LOGGER.info(f"Average tokens per sample: {train_dataset.tokens / len(train_dataset):.2f}")
         LOGGER.info(f"Using training config: {numerical_configs}")
         LOGGER.info(f"Initiating training with {'mixed-precision' if MIXED_PRECISION_ENABLED else 'single-precision'}...")
         LOGGER.info(f"Using model config: {model_config}")

utils.py
@@ -36,18 +36,28 @@ class EarlyStopping:
             if self.counter >= self.patience:
                 return True
         return False
-    
-def get_lookback_mask(size: int) -> torch.Tensor:
+
+@torch.no_grad() 
+def get_casual_mask(size: int) -> torch.Tensor:
     # Lower triangular matrix
     # [[
-    #   [1, 0, ... , 0],
-    #   [1, 1, ... , 0],
-    #   [1, 1, ... , 0],
-    #   [1, 1, ... , 1]
+    #   [True, False, ... , False],
+    #   [True, True,  ... , False],
+    #   [True, True,  ... , False],
+    #   [True, True,  ... , True ]
     # ]]
-    # size x size
-    return torch.triu(torch.ones(size, size), diagonal=1).type(torch.int) == 0
+    # 1 x size x size
+    idx = torch.arange(size, dtype=torch.int)
+    return (idx[None, :, None] >= idx[None, None, :]) # mask[i, j] = True if i ≥ j, else False.
 
+@torch.no_grad()
+def get_casual_and_prefix_mask(size: int, prefix_boundaries: list[tuple[int, int]]) -> torch.Tensor:
+    mask = get_casual_mask(size)
+    
+    for start, end in prefix_boundaries:
+        mask[0, start:, :end + 1] = True
+    
+    return mask
 
 def log_confidence_metrics(logits: torch.Tensor, targets: torch.Tensor, tb_logger: TensorboardLogger, global_step: int, ignore_index: int, log_interval: int=50):
     if global_step % log_interval == 0:
@@ -82,10 +92,10 @@ def validate(model: GPTmodel, data_loader: DataLoader, loss_func: nn.CrossEntrop
         # (N_BATCHES, SEQ_LEN)
         decoder_input: torch.Tensor = batch[0].to(DEVICE)
         label: torch.Tensor         = batch[1].to(DEVICE)
-
-        # (SEQ_LEN, SEQ_LEN)
-        decoder_mask: torch.Tensor = get_lookback_mask(decoder_input.shape[1]).to(DEVICE)
-
+        
+        # (N_BATCHES, SEQ_LEN, SEQ_LEN)
+        decoder_mask: torch.Tensor  = batch[2].to(DEVICE)
+        
         with torch.autocast(DEVICE.type, enabled=MIXED_PRECISION_ENABLED):
             # (N_BATCHES, SEQ_LEN, VOCAB_SIZE)
             logits: torch.Tensor = model(decoder_input, decoder_mask)